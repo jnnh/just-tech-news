4300310e0444eacc59bdc2535631b779606cbf7c
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-03 19:01:36 -0500

    hashed passwords

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -22,7 +23,7 @@ User.init(
         // define a username column
         username: {
             type: DataTypes.STRING,
-            allowNull: false 
+            allowNull: false
         },
         // define an email column
         email: {
@@ -46,18 +47,30 @@ User.init(
         }
     },
     {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifestyle "hook" functinality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
 
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    //don't automatically create createdAT/updatedAT timestamp fields
-    timestamps: false,
-    //don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. 'comment_text' and not 'commentText')
-    underscored: true,
-    //make it so our model name stays lowercase in the database
-    modelName: 'user'
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        //don't automatically create createdAT/updatedAT timestamp fields
+        timestamps: false,
+        //don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. 'comment_text' and not 'commentText')
+        underscored: true,
+        //make it so our model name stays lowercase in the database
+        modelName: 'user'
     }
 );
 
